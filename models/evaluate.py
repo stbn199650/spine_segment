dd3ca89cef624e8b8d2d8ea05f7c17a26d5f2b13
stbn199650 <stbn199650@gmail.com>
2019-12-21 13:23:39 +0800

    Add files via upload

---
+++ models/evaluate.py
@@ -0,0 +1,107 @@
+# import glob
+from __future__ import division
+import os
+import numpy as np
+import skimage.io as io
+from skimage import transform
+import datetime
+"""
+根据比赛的要求，采用的评判标准，这个程序写的不一定标准，可能需要进一步验证
+
+四、评分标准
+Dice 相似性系数是用分割标准和提交结果之间的重叠体素量的两倍除以分
+割标准和提交结果中的体素量的总和。PPV 是来衡量 TP 与 FP 之间数量关系。
+Sensitivity 灵敏度用来计算 TP 和 FN 的量。我们将用 Dice 相似性系数(DSC)，
+Positive Predicted Value(PPV)和 Sensitivity3 个值对分割性能的进行评估
+DSC =2𝑇𝑃/(𝐹𝑃+2𝑇𝑃+𝐹𝑁)    (1)
+PPV =𝑇𝑃/(𝑇𝑃+𝐹𝑃)          (2)
+Sensitivity =𝑇𝑃/(𝑇𝑃+𝐹𝑁)  (3)
+其中 TP、FP 和 FN 分别表示真阳性、假阳性和假阴性的数量。
+"""
+
+def evalue_score(predict_image_path, predict_label_path):
+    image_names = os.listdir(predict_image_path)
+    label_names = os.listdir(predict_label_path)
+    records = []   # 记录内容[ssi, all_acc, label_src.size, TP, FP, FN]
+    print("evalue predict ...")
+    for image_name in image_names:
+        label = "label_" + "_".join(image_name.split("_")[2:4])
+        if label in label_names:
+            ssi = predict_image_path+image_name
+            ssl = predict_label_path+label
+            predict_image = io.imread(ssi, as_gray=True)
+            label_src = io.imread(ssl, as_gray=True)
+            if predict_image.shape[0] != label_src.shape[0]:
+                # print(ssi)
+                # print(label_src.shape[0])
+                # print("PPV:", PPV)
+                # io.imshow(predict_image)
+                # io.show()
+                # print(predict_image[500])
+                predict_image = transform.resize(predict_image, (label_src.shape[0], label_src.shape[1]), mode='constant')
+                predict_image[predict_image >= 0.5] = 255
+                predict_image[predict_image < 0.5] = 0
+                # io.imshow(predict_image)
+                # io.show()
+            print("MAX:", np.max(predict_image))
+            predict_image = predict_image/np.max(predict_image)
+            label_src = label_src/255
+            predict_one = np.sum(predict_image[:] == 1)
+            label_one = np.sum(label_src[:] == 1)
+            predict_image = (predict_image == 1)
+            label_src = (label_src == 1)
+            true_matrix = np.bitwise_and(predict_image, label_src)
+            all_acc = np.sum(predict_image[:] == label_src[:])
+            ACC = all_acc/label_src.size
+            TP = np.sum(true_matrix[:])
+            FP = predict_one - TP
+            FN = np.sum(np.bitwise_xor(true_matrix, label_src)[:])
+            DSC = (2 * TP) / (FP + 2 * TP + FN)
+            PPV = TP / (TP + FP)
+            SEN = TP / (TP + FN)#记录内容[ssi, all_acc, label_src.size, TP, FP, FN]
+            records.append([ssi, all_acc, label_src.size, TP, FP, FN])
+            # print("score:", '%.2f' % ACC, '%.2f' % DSC, '%.2f' % PPV, '%.2f' % SEN)
+            # io.imshow(label_src)
+            # io.show()
+    return records
+
+
+if __name__ == '__main__':
+    valid_label = "../data/spine/valid/label/"
+    valid_image = "../data/spine/test/predict/"
+    result_save_path = "../data/results/"
+    score = evalue_score(valid_image, valid_label)
+    acc = 0
+    size = 0
+    TP = 0
+    FP = 0
+    FN = 0
+    for single_score in score:
+        # print(single_score)#[ssi, all_acc, label_src.size, TP, FP, FN]
+        # acc, size, TP, FP, FN = single_score[1:]
+        acc += single_score[1]
+        size += single_score[2]
+        TP += single_score[3]
+        FP += single_score[4]
+        FN += single_score[5]
+        # print(acc, size, TP, FP, FN)
+    DSC = (2 * TP) / (FP + 2 * TP + FN)
+    PPV = TP / (TP + FP)
+    SEN = TP / (TP + FN)
+    ACC = acc / size
+    SCORE = (DSC+PPV+SEN)/3
+    print("score:", '%.2f' % SCORE)
+    print("ACC:", '%.2f' % ACC)
+    print("DSC:", '%.2f' % DSC)
+    print("PPV:", '%.2f' % PPV)
+    print("SEN:", '%.2f' % SEN)
+    nowTime = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M_')
+    with open(result_save_path + str(nowTime) + str('{:.2f}'.format(ACC * 100)) + ".txt", 'w') as f:
+        f.writelines('model:unet\n')
+        f.writelines('DSC:' + str(DSC) + '\n')
+        f.writelines('PPV:' + str(PPV) + '\n')
+        f.writelines('SEN:' + str(SEN) + '\n')
+        f.writelines('ACC:' + str(ACC) + '\n')
+        f.writelines('SCORE:' + str(SCORE) + '\n')
+
+
---
+++ models/train.py
@@ -0,0 +1,134 @@
+from model import *
+from load_data import *
+from model1 import *
+import tensorflow as tf
+from keras.callbacks import ModelCheckpoint
+import matplotlib.pyplot as plt
+import keras
+import datetime
+from keras.backend.tensorflow_backend import set_session
+
+# config = tf.ConfigProto(allow_soft_placement=True)
+# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
+# config.gpu_options.allow_growth = True
+
+config = tf.ConfigProto()
+config.gpu_options.allocator_type = 'BFC'
+config.gpu_options.per_process_gpu_memory_fraction = 0.7
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+model_save_path = '../data/save_models/'
+savemodel_threshold = 0.9825  # 保存模型的最低准确率
+max_acc = 0   # 用来控制保存最高精度模型
+
+
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = {'batch': [], 'epoch': []}
+        self.accuracy = {'batch': [], 'epoch': []}
+        self.val_loss = {'batch': [], 'epoch': []}
+        self.val_acc = {'batch': [], 'epoch': []}
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses['batch'].append(logs.get('loss'))
+        self.accuracy['batch'].append(logs.get('acc'))
+        self.val_loss['batch'].append(logs.get('val_loss'))
+        self.val_acc['batch'].append(logs.get('val_acc'))
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses['epoch'].append(logs.get('loss'))
+        self.accuracy['epoch'].append(logs.get('acc'))
+        self.val_loss['epoch'].append(logs.get('val_loss'))
+        self.val_acc['epoch'].append(logs.get('val_acc'))
+        global max_acc
+        if logs.get('val_acc') > savemodel_threshold and logs.get('val_acc') > max_acc:
+            nowTime1 = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M_')
+            model_name = model_save_path + str(nowTime1) + str('{:.2f}'.format(logs.get('val_acc') * 100))
+            with open(model_name + ".txt", 'w') as f:
+                f.writelines('model:unet\n')
+                f.writelines('Train epoch:\n'+str(len(self.val_acc['epoch'])))
+            f.close()
+            model.save(model_name+'.h5')
+            max_acc = logs.get('val_acc')
+
+    def loss_plot(self, loss_type):
+        iters = range(len(self.losses[loss_type]))
+        plt.figure()
+        # acc
+        plt.plot(iters, self.accuracy[loss_type], 'r', label='train acc')
+        # loss
+        # plt.plot(iters, self.losses[loss_type], 'g', label='train loss')
+        if loss_type == 'epoch':
+            # val_acc
+            plt.plot(iters, self.val_acc[loss_type], 'b', label='val acc')
+            # val_loss
+        #    plt.plot(iters, self.val_loss[loss_type], 'k', label='val loss')
+        plt.grid(True)
+        plt.xlabel(loss_type)
+        plt.ylabel('acc')
+        plt.legend(loc="upper right")
+        print('max valid:', max(self.val_acc['epoch']))
+        global max_valid_result
+        max_valid_result = max(self.val_acc['epoch'])
+        plt.figure()
+        plt.plot(iters, self.losses[loss_type], 'g', label='train loss')
+        plt.plot(iters, self.val_loss[loss_type], 'k', label='val loss')
+        plt.grid(True)
+        plt.xlabel(loss_type)
+        plt.ylabel('loss')
+        plt.legend(loc="upper right")
+        plt.show()
+
+
+if __name__ == '__main__':
+    train_path = "../data/spine/train/"
+    valid_path = "../data/spine/valid/"
+    train_data = "../data/spine/train/image/"
+    train_label = "../data/spine/train/label/"
+    test_data = "../data/spine/test/image1/"
+    predict_path = "../data/spine/test/predict/"
+    # train_data = "../data/spine/train/image1/"
+    # train_label = "../data/spine/train/label1/"
+
+    data_gen_args = dict(samplewise_std_normalization=False, samplewise_center=True,  rotation_range=1, width_shift_range=0.05, height_shift_range=0.2,
+                         shear_range=0.05, zoom_range=0.05, horizontal_flip=True, fill_mode='constant', cval=0)
+
+    trainGen = trainGenerator(1, train_path, 'image', 'label', data_gen_args, save_to_dir=False)
+    validGen = trainGenerator(1, valid_path, 'image', 'label', data_gen_args, save_to_dir=False)
+    class_weight = [0.4, 0.6]
+    model = unet_plus(880, 880, color_type=1, num_class=1, deep_supervision=True)
+    # model = unet(model_save_path + "2019-05-22_07-13_98.25.h5", input_size=(512, 512, 1), class_weights=class_weight)
+    # model = unet(input_size=(880, 880, 1), class_weights=class_weight)
+    # model = get_unet_model(filters=32, input_size=(880, 880, 1))
+    # model_checkpoint = ModelCheckpoint(model_save_path+"unet_spine.hdf5", monitor='loss', verbose=2, save_best_only=True)
+    history = LossHistory()
+    #steps_per_epoch=2208,epochs=20
+    model.fit_generator(trainGen, steps_per_epoch=100, validation_data=validGen, validation_steps=252,
+                        epochs=5, verbose=1, callbacks=[history])  # steps_per_epoch=2208,validation_steps=252
+
+
+    history.loss_plot('epoch')
+
+    # model_checkpoint = ModelCheckpoint(model_save_path + "unet_spine_all.hdf5", monitor='loss', verbose=2,
+    #                                    save_best_only=True)
+    # model.fit_generator(trainGen, steps_per_epoch=2460, epochs=20, verbose=1, callbacks=[model_checkpoint])
+
+
+    def weighted_binary_crossentropy(y_true, y_pred):
+        class_loglosses = K.mean(K.binary_crossentropy(y_true, y_pred), axis=[0, 1, 2])
+        return K.sum(class_loglosses * K.constant(class_weight))
+    # model = load_model(model_save_path + "unet_spine.hdf5", custom_objects={'weighted_binary_crossentropy': weighted_binary_crossentropy})
+
+
+    predict_num = 10
+    predict_all_flag = True
+    predict_data_path = "../data/spine/test/image/"
+    images = os.listdir(predict_data_path)
+    if predict_all_flag is True or predict_num > len(images):
+        predict_num = len(images)
+    testGene = testGenerator(predict_data_path, predict_num, predict_all_flag)
+
+    results = model.predict_generator(testGene, predict_num, verbose=1)
+    saveResult(predict_path, predict_data_path, results)
+
